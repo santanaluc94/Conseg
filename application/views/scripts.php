6c913ded3a38ab53fd7c0a06dd6e534eaa7b83a1
Lucas Santana <santanaluc94@gmail.com>
2020-02-12 23:30:20 -0300

    Criando template padrão

---
+++ application/views/scripts.php
@@ -0,0 +1,8 @@
+    <!-- FontAwesome JS-->
+    <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js" integrity="sha384-0pzryjIRos8mFBWMzSSZApWtPl/5++eIfzYmTgBBmXYdhvxPc+XcFEk+zJwDgWbP" crossorigin="anonymous">
+    </script>
+
+    <!-- Boostrap JS -->
+    <script type="text/javascript" src="public/bootstrap/js/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="public/bootstrap/js/jquery.mask.min.js"></script>
+    <script type="text/javascript" src="public/bootstrap/js/bootstrap.min.js"></script>
