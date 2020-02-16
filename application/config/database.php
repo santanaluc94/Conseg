be6b95de93386993bd36ddb65fb8b2c2995bb216
Wellington Almeida <wellingtonfinazzi@gmail.com>
2020-02-16 12:51:42 -0300

    Banco de Dados

--- application/config/database.php
+++ application/config/database.php
@@ -73,12 +73,36 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+/*
+$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => '23.229.222.66',
+	'username' => 'wfinazzi',
+	'password' => 'Grotkowsk1',
+	'database' => 'consegguarulhos',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+*/
+
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => '',
+	'username' => 'root',
 	'password' => '',
-	'database' => '',
+	'database' => 'conseg',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
